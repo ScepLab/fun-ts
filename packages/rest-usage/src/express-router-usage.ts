8d9c39980b0f1c05a2ee09efc34d046b35b4f01e
Jonathan Philipps <38406134+jphilipps@users.noreply.github.com>
2022-07-18 16:03:10 +0200

    move rest usage example into its own project

--- packages/rest-usage/src/express-router-usage.ts
+++ packages/rest-usage/src/express-router-usage.ts
@@ -1,6 +1,6 @@
 import { Comment, RestApiExample } from "./rest-definition";
 
-import { createTypedRouter } from "../src/express";
+import { createTypedRouter } from "@fun-ts/rest/src/express";
 import express from "express";
 import { faker } from "@faker-js/faker";
 
@@ -9,7 +9,7 @@ export const userRouter = express.Router();
 const router = createTypedRouter<RestApiExample>(userRouter);
 
 // eslint-disable-next-line no-unused-vars
-router.get("/comments", async (req, res) => {
+router.get("/comments", async (_req, _res) => {
     return [...Array(faker.datatype.number({
         min: 50,
         max: 1000
@@ -23,13 +23,13 @@ router.get("/comments", async (req, res) => {
 });
 
 // eslint-disable-next-line no-unused-vars
-router.get("/comment/:id", async (req, res) => {
+router.get("/comment/:id", async (_req, _res) => {
     return [...Array(faker.datatype.number({
         min: 50,
         max: 1000
     }))].map((_, i) => ({
         id: i,
-        postId: req.params.id,
+        // postId: req.params.id,
         body: faker.lorem.words(),
         name: faker.lorem.words(),
         email: faker.internet.email(),
