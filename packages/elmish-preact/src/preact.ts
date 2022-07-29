7a5d88865ec5356b1a6351ef9295a585fd1d52f8
Jonathan Philipps <38406134+jphilipps@users.noreply.github.com>
2022-07-29 18:38:06 +0200

    minor style adaptions

--- packages/elmish-preact/src/preact.ts
+++ packages/elmish-preact/src/preact.ts
@@ -5,12 +5,11 @@ import { pipe } from "fp-ts/function";
 
 export type PreactView<Model, Msg> = View<Model, Msg, ComponentChild>;
 
-export const withPreactSynchronous =
-    (parent: HTMLElement) =>
-        <Arg, Model, Msg>(prog: Program<Arg, Model, Msg, ComponentChild>) =>
-            pipe(
-                prog,
-                program.withSetState<Model, Msg>((dispatch, model) => {
-                    render(prog.view(dispatch, model), parent);
-                })
-            );
+export const withPreactSynchronous = (parent: HTMLElement) =>
+    <Arg, Model, Msg>(prog: Program<Arg, Model, Msg, ComponentChild>) =>
+        pipe(
+            prog,
+            program.withSetState<Model, Msg>((dispatch, model) => {
+                render(prog.view(dispatch, model), parent);
+            }),
+        );
