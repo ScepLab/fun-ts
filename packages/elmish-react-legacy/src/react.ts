7e9b09645ad8c7d06cd949d48171b432abd494e5
Jonathan Philipps <38406134+jphilipps@users.noreply.github.com>
2022-08-01 15:13:26 +0200

    minor import adaptions

--- packages/elmish-react-legacy/src/react.ts
+++ packages/elmish-react-legacy/src/react.ts
@@ -1,8 +1,8 @@
-import { Program, program, View } from "@fun-ts/elmish";
+import { Program, View, program } from "@fun-ts/elmish";
 
-import ReactDOM from "react-dom";
 import { ReactElement } from "react";
 import { pipe } from "fp-ts/function";
+import { render } from "react-dom";
 
 export type ReactView<Model, Msg> = View<Model, Msg, ReactElement>;
 
@@ -10,6 +10,6 @@ export const withReactSynchronous = (parent: HTMLElement) =>
     <Arg, Model, Msg>(prog: Program<Arg, Model, Msg, ReactElement>) => pipe(
         prog,
         program.withSetState<Model, Msg>((dispatch, model) => {
-            ReactDOM.render(prog.view(dispatch, model), parent);
+            render(prog.view(dispatch, model), parent);
         }),
     );
