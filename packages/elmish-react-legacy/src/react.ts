10db58d3840e4dede94c6b864b9ffb1b334978fc
Jonathan Philipps <38406134+jphilipps@users.noreply.github.com>
2022-08-01 14:03:22 +0200

    add and export ReactView

--- packages/elmish-react-legacy/src/react.ts
+++ packages/elmish-react-legacy/src/react.ts
@@ -1,8 +1,10 @@
-import { Program, program } from "@fun-ts/elmish";
+import { Program, program, View } from "@fun-ts/elmish";
 
 import ReactDOM from "react-dom";
 import { ReactElement } from "react";
 import { pipe } from "fp-ts/function";
+
+export type ReactView<Model, Msg> = View<Model, Msg, ReactElement>;
 
 export const withReactSynchronous = (parent: HTMLElement) =>
     <Arg, Model, Msg>(prog: Program<Arg, Model, Msg, ReactElement>) => pipe(
